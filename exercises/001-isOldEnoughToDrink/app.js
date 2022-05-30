910f0c87ceef2407e21103375a65570ec68c2200
Facundo <facundogds@gmail.com>
2022-05-30 12:18:37 +0000

    Repaso1

--- exercises/001-isOldEnoughToDrink/app.js
+++ exercises/001-isOldEnoughToDrink/app.js
@@ -1,3 +1,10 @@
-function isOldEnoughToDrink(){
-    // your code here	
+function isOldEnoughToDrink(age){
+    if (age >= 21){
+        return true;
+    } else {
+        return false;
+    }
 }
+
+let output = isOldEnoughToDrink(21);
+console.log(output);
